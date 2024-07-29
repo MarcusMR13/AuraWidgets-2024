45ac62586cd56ffa6c33e333891d4b50960afe71
Marcus Marques <marcusnopb@gmail.com>
2024-07-29 15:54:59 -0300

    Alinhamentos, Stacks, Columns & Rows

--- lib/main.dart
+++ lib/main.dart
@@ -15,50 +15,65 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.purple,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                width: 50,
-                height: 50,
-              )
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: Colors.purple,
-                width: 50,
-                height: 50,
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(color: Colors.cyan, height: 50, width: 50,),
-              Container(color: Colors.pinkAccent, height: 50, width: 50,),
-              Container(color: Colors.yellow, height: 50, width: 50,),
-            ],
-          ),
-        ],
+      home: Container(
+        color: Colors.blueAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.purple,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.purple,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.yellow,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
